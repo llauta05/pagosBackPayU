afd574674b0d7a2681b1a351549f0862e5ce64bd
lautaro.landin <lautaro.landin@istea.com.ar>
2020-01-07 02:12:09 -0300

    init

---
+++ mkplacepagos/Controllers/ValuesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+using mkplacepagos.data;
+
+namespace mkplacepagos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<dataPartner> Get()
+        {
+            return GetDataPartner();
+        }
+        public dataPartner GetDataPartner() {
+            return new dataPartner
+            {
+                accountId = 512326,
+                apiKey = "4Vj8eK4rloUd272L48hsrarnUA",
+                merchantId = 508029
+            };
+        }
+
+        public static string GetSHA256(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+
+        // GET api/values/data
+        [HttpPost]
+        [Route("CreateSignature")]
+        public ActionResult<dataSignature> CreateSignature([FromBody] dataPagos data)
+        {
+            var dataPartner = GetDataPartner();
+            var key = dataPartner.apiKey + '~' + dataPartner.merchantId + '~' + data.referenceCode + '~' + data.amount + '~' + data.currency;
+            return new dataSignature {signature = GetSHA256(key) };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ mkplacepagos/data/data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace mkplacepagos.data
+{
+    [DataContract]
+    public class dataPagos
+    {
+        [DataMember(Name = "referenceCode")]
+        public string referenceCode { get; set; }
+        [DataMember(Name = "amount")]
+        public decimal amount { get; set; }
+        [DataMember(Name = "currency")]
+        public string currency { get; set; }
+        [DataMember(Name = "buyerEmail")]
+        public string buyerEmail { get; set; }
+    }
+    [DataContract]
+    public class dataPartner
+    {
+        [DataMember(Name = "merchantId")]
+        public int merchantId { get; set; }
+        [DataMember(Name = "accountId")]
+        public int accountId { get; set; }
+        [DataMember(Name = "apiKey")]
+        public string apiKey { get; set; }
+    }
+    [DataContract]
+    public class dataSignature
+    {
+        [DataMember(Name = "signature")]
+        public string signature { get; set; }
+    }
+}
